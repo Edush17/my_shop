52d707a76d892ea8ae5a35d35863fcccfc38fa00
edo-pc-fisso <edoardo.di.biagio@hotmail.it>
2025-01-25 20:59:43 +0100

    second commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  userId: string = 'Edoardo';
+  password: string = '';
+  autenticato: boolean = true;
+  errorMsg: string = 'Spiacenti, il nome utente o la password sono errati.';
+  succesMsg: string = 'Autenticazione effettuata con successo';
 
-  userId: string = "Edoardo"
-  password: string = "";
+  constructor(private route: Router) {}
 
-  gestAuth = () : void => {
+  ngOnInit(): void {}
+
+  gestAuth = (): void => {
     console.log(this.userId);
-  }
-
+    if (this.userId === 'Edoardo' && this.password === '123') {
+      this.route.navigate(['welcome'])
+      this.autenticato = true;
+    } else {
+      this.autenticato = false;
+    }
+  };
 }
